9f0e13929229a332cfb158758f45ecc5ab329059
gphoon <garrett.phoonswadi@gmail.com>
2017-12-20 23:17:52 -0500

    updated to check ends properly

--- advent_1.py
+++ advent_1.py
@@ -4,28 +4,30 @@ import sys
 
 sum = list()
 
+
 def verify_helper(file, match):
-    index = 1
+    index = 0
     for x in range(1, len(file)):
         if file[x] == match:
             index += 1
         else:
             break
-    #print("helper found: " + str(index))
+    # print("helper found: " + str(index))
     return index
 
+
 def verify_input_iterative(file):
-    x =0
-    end = len(file)-1
+    x = 0
+    end = len(file) - 1
     while x <= end:
         current = file[x]
-        if x == len(file)-1:
+        if x == len(file) - 1:
             return
-        next = file[x+1]
+        next = file[x + 1]
         if current == next:
             check_for_more = verify_helper(file[x:], next)
-            if check_for_more > 2:
-                sum.append(int(file[x]) * check_for_more)
+            if check_for_more > 1:
+                sum.append(int(file[x])*check_for_more)
                 x += check_for_more
             else:
                 sum.append(file[x])
@@ -33,12 +35,12 @@ def verify_input_iterative(file):
         else:
             x += 1
 
+
 def verify_input_recursion(file, start):
-    #print("file: " + str(file))
-    #print("index: %d" % start)
-    if len(file) == 1 or len(file)==0:
+    # print("file: " + str(file))
+    # print("index: %d" % start)
+    if len(file) == 1 or len(file) == 0:
         return
-    start
     current = file[start]
     if start == len(file):
         next = file[-1]
@@ -47,7 +49,7 @@ def verify_input_recursion(file, start):
     if current == next:
         diff = verify_helper(file[start:], next)
         if diff > 2:
-            sum.append(int(file[start])*diff)
+            sum.append(int(file[start]) * diff)
             verify_input_recursion(file[start + diff:], 0)
         else:
             sum.append(file[start])
@@ -66,16 +68,15 @@ def check_ends(a):
 if __name__ == "__main__":
     with open(sys.argv[1]) as f:
         input = [line.strip() for line in f]
-    #print("input: " + str(input))
+    # print("input: " + str(input))
     a = list()
     for x in str(input):
         if x == '\'' or x == '[' or x == ']':
             continue
         a.append(x)
-    print("new input: " + str(a))
+    #print("new input: " + str(a))
     verify_input_iterative(a)
-    if len(sum) < 1:
-        check_ends(a)
+    check_ends(a)
     print("sum: " + str(sum))
     total = 0
     for x in sum:
