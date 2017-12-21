6f68f2b84bb39f20c853f3e40bbaf5f041297611
Garrett Phoonswadi <garrett.phoonswadi@gmail.com>
2017-12-21 14:15:26 -0500

    Make this code neater

--- Day_1/advent_1_2.py
+++ Day_1/advent_1_2.py
@@ -2,45 +2,30 @@
 
 import sys
 
-sum = list()
 
-def verify_input_iterative(file):
+def verify_input_iterative(sequence, total=0):
     x = 0
-    end = len(file)-1
-    while x <= end/2:
-        if x == (len(file)-1)/2:
-            return
-        current = file[x]
-        next_pos = ((len(file)/2) + x) % len(file)
-        next = file[next_pos]
-        if current == next:
-                sum.append(int(file[x])*2)
-                x += 1
-        else:
-            x += 1
+    end = len(sequence) - 1
+    while x <= end / 2:
+        next_pos = ((len(sequence) / 2) + x) % len(sequence)
+        if sequence[x % len(sequence)] == sequence[next_pos]:
+            total += (int(sequence[x]) * 2)
+        x += 1
+    check_ends(sequence, total)
+    return total
 
 
-def check_ends(a):
-    if a[-1] == a[(len(a)/2)-1]:
-        end = a[-1]
-        mid = a[(len(a)/2)-1]
-        sum.append(int(a[-1])*2)
+def check_ends(a, total):
+    if a[-1] == a[(len(a) / 2) - 1]:
+        total += (int(a[-1]) * 2)
 
 
 if __name__ == "__main__":
     with open(sys.argv[1]) as f:
-        input = [line.strip() for line in f]
+        sequence = [line.strip() for line in f]
     # print("input: " + str(input))
-    a = list()
-    for x in str(input):
-        if x != '\'' and x != '[' and x != ']':
-            a.append(x)
-    #print("new input: " + str(a))
-    verify_input_iterative(a)
-    check_ends(a)
-    #print("sum: " + str(sum))
-    total = 0
-    for x in sum:
-        total += int(x)
+    inputs = list()
+    [inputs.append(x) if x != '\'' and x != '[' and x != ']' else "" for x in str(sequence)]
+    # print("new input: " + str(inputs))
+    total = verify_input_iterative(inputs)
     print("total = " + str(total))
-
